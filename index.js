eb78e0609dbfddce99aba7c3e8cd0414d1aca8ae
urmila2801 <urmilapamarthi3@gmail.com>
2023-03-26 08:33:51 +0000

    practice-2

---
+++ index.js
@@ -0,0 +1,13 @@
+const element = (
+  <div>
+    <h1>Congratulations</h1>
+    <div className="container">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h2>Kiran V</h2>
+      <p>Vishnu Institute of Computer Education and Technology, Bhimavaram</p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
